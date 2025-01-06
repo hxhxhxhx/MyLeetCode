caf0d1f828c541ac5f15b856a8dca5f7143b03c6
huaxi <huaxiti@163.com>
2025-01-07 00:06:37 +0800

    test commit 3

--- src/main/java/test/jichu/tt1.java
+++ src/main/java/test/jichu/tt1.java
@@ -27,7 +27,7 @@ public class tt1 implements tt{
 //        for (;;){
 //
 //        }
-        System.out.println("2");
+        System.out.println("3");
         System.out.println("jj");
         boolean b = true;
 //        while(b){
